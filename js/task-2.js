0defc89fb38208670147501c9b732adb6020fffb
YevheniiaYakymenko <152288008+YevheniiaYakymenko@users.noreply.github.com>
2024-03-29 23:54:56 +0100

    task1,2

--- js/task-2.js
+++ js/task-2.js
@@ -1,3 +1,19 @@
+class Storage {
+  #items;
+  constructor(newItems) {
+    this.#items = newItems;
+  }
+  getItems() {
+    return this.#items;
+  }
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    this.#items = this.#items.filter(item => item !== itemToRemove);
+  }
+}
+
 const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 storage.addItem('Droid');
